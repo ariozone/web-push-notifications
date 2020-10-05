d64064eacad55e9b35eddb87da55ff3303ca8687
Ario Tamadoni <ariozone6@gmail.com>
2020-10-05 14:19:40 -0700

    Setup end points.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const webpush = require("web-push")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const port = app.set("port", process.env.PORT || 3001)
+import keys from "./keys.json"
 app.use(cors())
 app.use(bodyParser.json())
 app.get("/", (req, res) => {
@@ -11,4 +12,19 @@ app.get("/", (req, res) => {
 })
 app.listen(port, () => {
   console.log("Server is running on port: ", port)
+})
+
+webpush.setVapidDetails(
+  "mailto:ario@mycarauction.com",
+  keys["Public Key"],
+  keys["Public Key"]
+)
+app.post("/subscribe", (req, res) => {
+  const { subscription, title, message } = req.body
+  const payload = JSON.stringify({ title, message })
+
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error("err", err))
+  res.status(200).json({ success: true })
 })
